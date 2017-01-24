0126070d56eae0d85f60fc6fb1d05f3c60457e17
CJW <cwal37@gmail.com>
2017-01-23 23:26:35 -0500

    more iteration
    mostly in the data background, some excel work

---
+++ shapeid_map_loop.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Connor
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib.mlab import griddata
+#from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+from scipy.spatial import cKDTree as KDTree
+import pdb
+
+import matplotlib as mpl
+from matplotlib.font_manager import FontProperties
+import matplotlib.patheffects as path_effects
+
+plt.close()
+mpl.rcdefaults()
+mpl.rcParams['figure.figsize'] = 14, 9
+
+zones = pd.read_csv('congress_shapes_final.csv')
+shapeids = list(set(zones['shapeid']))
+
+"""
+no, no,no I need to use a patches collection
+remember to do this tomorrow, not gonna finish tonight
+I need to sleep so that I can work and still do this after
+
+"""
+
+for sid in shapeids:
+    sid_df = zones[zones['shapeid'] == sid]
+    plt.plot(sid_df['x'], sid_df['y'], color = 'k', linewidth = 0.1)
+#plt.show()
+#print(t)
+plt.savefig('congress_test.png', dpi=500)
